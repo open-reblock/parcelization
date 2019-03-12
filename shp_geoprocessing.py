7014b731bc7cc17243fdfeaa1bb8d4c517707353
ayang861 <47541447+ayang861@users.noreply.github.com>
2019-03-12 01:27:02 -0500

    generalized variables

--- shp_geoprocessing.py
+++ shp_geoprocessing.py
@@ -15,77 +15,81 @@ from shapely.geometry import JOIN_STYLE
 import matplotlib.pyplot as plt 
 import pytess
 
+bldgs_filename = 'hoima_bldgs.shp'
+bounds_filename = 'hoima_bound1.shp'
 
 os.chdir('c:\\Users\\Annie\\Downloads')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', -1)
 
-hoima_set = gpd.read_file("hoima_bldgs.shp")
-hoima_set = hoima_set.to_crs(epsg = 3857)
-#hoima_set.plot()
+set_bldgs = gpd.read_file(bldgs_filename)
+set_bldgs = set_bldgs.to_crs(epsg = 3857)
+#set_bldgs.plot()
 
-hoima_bounds = gpd.read_file('hoima_bound1.shp')
-hoima_bounds = hoima_bounds.to_crs(epsg=3857)
-hoima_bounds
-hoima_bounds = hoima_bounds.plot(alpha = .1)
-hoima_set.plot(ax = hoima_bounds)
+set_bounds = gpd.read_file(bounds_filename)
+set_bounds = set_bounds.to_crs(epsg=3857)
+
+#set_bounds = set_bounds.plot(alpha = .1)
+#set_bldgs.plot(ax = set_bounds)
+
 "find intersect of bound and building shp, create new shp"
 bldgs_inbnds = []
-for index1, sets in hoima_set.iterrows():
-    for index2, bounds in hoima_bounds.iterrows():
+for index1, sets in set_bldgs.iterrows():
+    for index2, bounds in set_bounds.iterrows():
        if sets['geometry'].intersects(bounds['geometry']):
           bldgs_inbnds.append({'geometry': sets['geometry'].intersection(bounds['geometry'])})
 bldgs_inbnds = gpd.GeoDataFrame(bldgs_inbnds,columns=['geometry'])
 bldgs_inbnds.to_file('intersection.shp')
 
-e"performing geoprocessing on buildings in ona bounds"
-hoima_set = gpd.read_file('intersection.shp')
-hoima_set.crs = {'init': 'epsg:3857'}
-hoima_buff = hoima_set.buffer(5)
+"performing geoprocessing on buildings in ona bounds"
+set_intersect = gpd.read_file('intersection.shp')
+set_intersect.crs = {'init': 'epsg:3857'}
+set_buff = set_intersect.buffer(5)
 #hoima_buff.plot(edgecolor = 'black')
 
-hoima_diss = hoima_buff.unary_union
-hoima_diss= gpd.GeoDataFrame(crs = hoima_buff.crs, geometry = [hoima_diss])
-hoima_diss.plot(edgecolor= 'black')
+set_diss = set_buff.unary_union
+set_diss= gpd.GeoDataFrame(crs = set_buff.crs, geometry = [set_diss])
+#set_diss.plot(edgecolor= 'black')
 
-hoima_cent = hoima_set.copy()
-hoima_cent.geometry = hoima_cent.centroid
-hoima_cent.crs = hoima_set.crs
-hoima_cent
+set_cent = set_intersect.copy()
+set_cent.geometry = set_cent.centroid
+set_cent.crs = set_intersect.crs
+set_cent
 
-hoima_x = hoima_cent.geometry.x
-hoima_y = hoima_cent.geometry.y
+set_x = set_cent.geometry.x
+set_y = set_cent.geometry.y
 
 
 #hoima_xy = [ [x,y] for (x,y) in list(zip(hoima_x, hoima_y))]
-hoima_xy = np.array(list(tuple(zip(hoima_x, hoima_y))))
-hoima_xy
+set_xy = np.array(list(tuple(zip(set_x, set_y))))
+set_xy
 
-hoima_vor = spatial.Voronoi(hoima_xy)
-spatial.voronoi_plot_2d(hoima_vor)
-plt.show()
+set_vor = spatial.Voronoi(set_xy)
+spatial.voronoi_plot_2d(set_vor)
+#plt.show()
 
-hoima_lines = [  
-        shapely.geometry.LineString(hoima_vor.vertices[line])
-        for line in hoima_vor.ridge_vertices
+set_lines = [  
+        shapely.geometry.LineString(set_vor.vertices[line])
+        for line in set_vor.ridge_vertices
         if -1 not in line
 ]
-hoima_lines
+set_lines
 
-hoima_vor_poly = list(shapely.ops.polygonize(hoima_lines))
-print(hoima_vor_poly)
-hoima_vor_gdf = gpd.GeoDataFrame(crs = {'init': 'epsg:3857'}, geometry = hoima_vor_poly)
-hoima_vor_gdf.plot()
+set_vor_poly = list(shapely.ops.polygonize(set_lines))
+print(set_vor_poly)
+set_vor_gdf = gpd.GeoDataFrame(crs = {'init': 'epsg:3857'}, geometry = set_vor_poly)
+#set_vor_gdf.plot()
 
 
 #hoima_vor = pytess.voronoi(hoima_xy)
 #hoima_vorpolys = [Polygon(hoima_vor[0][1]) for i in hoima_vor]
 #hoima_vor_gdf = gpd.GeoDataFrame(crs = {'init': 'epsg:3857'}, geometry = hoima_vorpolys)
 #hoima_vor_gdf.plot()
+
 'intersecting dissolved settlment w voronoi tess'
 vor_diss = []
-for index1, sets in hoima_diss.iterrows():
-    for index2, bounds in hoima_vor_gdf.iterrows():
+for index1, sets in set_diss.iterrows():
+    for index2, bounds in set_vor_gdf.iterrows():
        if sets['geometry'].intersects(bounds['geometry']):
           vor_diss.append({'geometry': sets['geometry'].intersection(bounds['geometry'])})
 vor_diss = gpd.GeoDataFrame(vor_diss,columns=['geometry'])
@@ -95,7 +99,7 @@ vor_diss = gpd.read_file('intsct_vor.shp')
 vor_diss.plot(edgecolor = 'black')
 vor_diss
 
-'cleaning up non-overlapping buffers'
+'cleaning up non-overlapping buffers- wip!!!!'
 eps = .01
 fx = vor_diss.buffer(eps, 1, join_style=JOIN_STYLE.mitre).buffer(-eps, 1, join_style=JOIN_STYLE.mitre)
 fx.plot()
